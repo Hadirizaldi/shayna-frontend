a7a7d92e203287d5b5b01429ca04fb3455593ba6
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-10 16:11:49 +0700

    edit the logo in the header and footer files so that it connects to the home page using the router link

--- src/components/footer/FooterMain.vue
+++ src/components/footer/FooterMain.vue
@@ -6,7 +6,9 @@
         <div class="col-lg-5">
           <div class="footer-left">
             <div class="footer-logo">
-              <a href="#"><img src="/img/logo_website_shayna_white.png" alt="" /></a>
+              <router-link to="/">
+                <img src="/img/logo_website_shayna_white.png" alt="" />
+              </router-link>
             </div>
             <ul>
               <li>Address: Setra Duta, Bandung</li>
