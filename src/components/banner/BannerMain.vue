2cabab200bd2e1078b79130e9433acaec3158cdc
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-10 14:52:09 +0700

    banner main done

---
+++ src/components/banner/BannerMain.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="women-banner spad">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 mt-5">
+          <Carousel class="product-slider" :items-to-show="3">
+            <slide v-for="slide in 6" :key="slide">
+              <div class="product-item">
+                <div class="pi-pic">
+                  <img src="/img/mickey1.jpg" alt="" />
+                  <ul>
+                    <li class="w-icon active">
+                      <a href="#"><i class="icon_bag_alt"></i></a>
+                    </li>
+                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                  </ul>
+                </div>
+                <div class="pi-text">
+                  <div class="catagory-name">Coat</div>
+                  <a href="#">
+                    <h5>Mickey Baggy</h5>
+                  </a>
+                  <div class="product-price">
+                    $14.00
+                    <span>$35.00</span>
+                  </div>
+                </div>
+              </div>
+            </slide>
+            <template #addons>
+              <pagination />
+            </template>
+          </Carousel>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// for carousel
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination } from 'vue3-carousel'
+
+export default {
+  name: "BannerMain",
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+  }
+}
+</script>
+
+<style scoped>
+.product-item {
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,23 @@
 <template>
   <header-main />
   <hero-main />
+  <banner-main />
 </template>
 
 <script>
 
 import HeaderMain from '@/components/header/HeaderMain.vue';
 import HeroMain from '@/components/hero/HeroMain.vue';
+import BannerMain from '@/components/banner/BannerMain.vue';
+
 
 export default {
   name: "HomeView",
 
   components: {
     HeaderMain,
-    HeroMain
+    HeroMain,
+    BannerMain
   }
 }
 </script>
